5462e24e08ea99b7a5eace1042e751aaa693e2f1
chaejin-jen <jen71324@gmail.com>
2023-07-12 11:51:41 +0900

    🎉

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <RouterLink to="/chat/channel1">channel1</RouterLink>
+    <br />
+    <RouterLink to="/chat/channel2">channel2</RouterLink>
+    <br />
+    <RouterLink to="/chat/channel3">channel3</RouterLink>
+  </main>
+</template>
